3c5282b296712bce2ea7b9f97222c751f4f28411
Gabriel Miranda <gabrielmfern@outlook.com>
2025-07-30 10:40:49 -0300

    add layout component to reuse

---
+++ src/components/layout.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const glue = @import("../glue.zig");
+
+pub fn layout(
+    comptime title: []const u8,
+    children: anytype,
+) std.meta.Tuple(&.{
+    []const u8,
+    []const u8,
+    []const u8,
+    []const u8,
+    []const u8,
+    []const u8,
+    []const u8,
+    []const u8,
+    @TypeOf(children),
+    []const u8,
+    []const u8,
+}) {
+    return .{
+        "<!DOCTYPE html>",
+        "<html lang=\"en\">",
+        "<head>",
+        "  <meta charset=\"UTF-8\">",
+        "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">",
+        "  <title>" ++ title ++ "</title>",
+        "</head>",
+        "<body>",
+        children,
+        "</body>",
+        "</html>",
+    };
+}
--- src/routes/root.zig
+++ src/routes/root.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const http = std.http;
 const glue = @import("../glue.zig");
+const layout = @import("../components/layout.zig").layout;
 
 const use_allocator = @import("../main.zig").use_allocator;
 
@@ -12,21 +13,14 @@ pub const predicate = glue.Predicates.exact(
 pub fn handler(request: *http.Server.Request) anyerror!void {
     const allocator = use_allocator();
     const html = try glue.html(.{
-        \\ <!DOCTYPE html>
-        \\ <html lang="en">
-        \\ <head>
-        \\ <meta charset="UTF-8">
-        \\ <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        \\ <title>Welcome to Zig HTTP Server</title>
-        \\ </head>
-        \\ <body>
-        \\ <h1>Welcome to Zig HTTP Server</h1>
-        \\ <p>This is a simple HTTP server written in Zig.</p>
-        \\ <p>Feel free to explore the code and modify it as you wish!</p>
-        ,
-        "<p>Current elapsed time: ", std.time.timestamp(), "</p>",
-        \\ </body>
-        \\ </html>
+        layout("mypet", .{
+            "<h1>Welcome to Zig HTTP Server</h1>",
+            "<p>This is a simple HTTP server written in Zig.</p>",
+            "<p>Feel free to explore the code and modify it as you wish!</p>",
+            "<p>Current elapsed time: ",
+            std.time.timestamp(),
+            "</p>",
+        }),
     }, allocator);
     defer allocator.free(html);
     try request.respond(
