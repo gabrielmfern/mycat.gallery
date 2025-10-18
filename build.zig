284f1d32fc6c12c0bf804c0fb5304b8fe680c6b1
gabriel miranda <gabrielmfern@outlook.com>
2025-10-18 08:09:20 -0300

    improve build script

--- build.zig
+++ build.zig
@@ -12,11 +12,17 @@ pub fn build(b: *std.Build) !void {
         }),
     });
     const generate_routes_step = b.addRunArtifact(generate_routes);
-    var update_source_files = b.addUpdateSourceFiles();
-    update_source_files.addCopyFileToSource(
+    var wf = b.addUpdateSourceFiles();
+    wf.addCopyFileToSource(
         generate_routes_step.addOutputFileArg("routes.zig"),
         "./src/routes.zig",
     );
+
+    const glue = b.createModule(.{
+        .root_source_file = b.path("glue.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
@@ -25,11 +31,7 @@ pub fn build(b: *std.Build) !void {
     });
     exe_mod.addImport(
         "glue",
-        b.createModule(.{
-            .root_source_file = b.path("glue.zig"),
-            .target = target,
-            .optimize = optimize,
-        }),
+        glue,
     );
     exe_mod.addImport(
         "sqlite",
@@ -47,21 +49,29 @@ pub fn build(b: *std.Build) !void {
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(&update_source_files.step);
+    run_cmd.step.dependOn(&wf.step);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
     const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&wf.step);
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
+    const webserver_tests = b.addTest(.{
         .root_module = exe_mod,
     });
+    webserver_tests.step.dependOn(&wf.step);
+    const run_webserver_tests = b.addRunArtifact(webserver_tests);
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const glue_tests = b.addTest(.{
+        .root_module = glue,
+    });
+    const run_glue_tests = b.addRunArtifact(glue_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&wf.step);
+    test_step.dependOn(&run_glue_tests.step);
+    test_step.dependOn(&run_webserver_tests.step);
 }
