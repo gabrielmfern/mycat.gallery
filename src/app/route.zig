babfede2be025b8863ffd953cab2789476087843
Gabriel Miranda <gabrielmfern@outlook.com>
2025-10-18 11:46:11 -0300

    add favorite button

--- src/app/route.zig
+++ src/app/route.zig
@@ -19,12 +19,19 @@ pub fn handler(request: *http.Server.Request) anyerror!void {
     var picture_elements = try allocator.alloc([]const u8, page.pictures.len);
     for (page.pictures, 0..) |picture, i| {
         picture_elements[i] = try glue.html(.{
-            "<div class=\"picture-card\">",
+            "<div class=\"picture-card relative group\">",
             "   <img",
             "       src=\"",
             picture.uri,
             "\"",
             "   />",
+            "   <button class=\"absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity p-2 bg-transparent text-brown-1 appearence-none border-none\" data-favorite=\"",
+            "\">",
+            if (picture.favorite)
+                "       <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 24 24\"><path fill=\"currentColor\" d=\"m12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5C2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z\"/></svg>"
+            else
+                "       <svg xmlns=\"http://www.w3.org/2000/svg\" width=\"20\" height=\"20\" viewBox=\"0 0 24 24\"><path fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" d=\"m12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5C2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z\"/></svg>",
+            "   </button>",
             "</div>",
         }, allocator);
     }
